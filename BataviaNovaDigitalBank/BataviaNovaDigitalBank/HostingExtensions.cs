5dffc710522db32e8e1475f1363bc77b4f11dc29
Bas van Uggelen <isabblex@gmail.com>
2026-03-14 11:53:54 +0100

    - update to non vulnerable version

--- BataviaNovaDigitalBank/BataviaNovaDigitalBank/HostingExtensions.cs
+++ BataviaNovaDigitalBank/BataviaNovaDigitalBank/HostingExtensions.cs
@@ -41,19 +41,6 @@ namespace BataviaNovaDigitalBank
             //builder.Services.Configure<RazorPagesOptions>(options =>
             //    options.Conventions.AuthorizeFolder("/ServerSideSessions", "admin"));
 
-
-            builder.Services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
-
             return builder.Build();
         }
 
